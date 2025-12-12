d0fb2183bb8836e3a828e7415afc37700fa9a284
Anirudh <anirudhkandakatla1@gmail.com>
2025-12-12 20:45:51 +0530

    kndjksjk

--- src/test/java/Temp/GenerateEmailDemo.java
+++ src/test/java/Temp/GenerateEmailDemo.java
@@ -6,6 +6,8 @@ public class GenerateEmailDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+// test data
+
 		
 		
 //		return new Date().toString().replaceAll(" ", "").replaceAll("\\:", "")+"@gamil.com";
